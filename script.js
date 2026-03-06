a100eee32406b7b93a10deb8411085617297172f
brondo138 <lovosalex0@gmail.com>
2026-03-06 10:26:35 -0600

    terminado

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
+//Varibles tomadas del index
 const form = document.getElementById("formCurso");
 const lista = document.getElementById("listaCursos");
 const contador = document.getElementById("contador");
 const error = document.getElementById("error");
-
 let totalCursos = 0;
 
+//funcion que actualiza contador
 function actualizarContador(){
 contador.textContent = "Cursos disponibles: " + totalCursos;
 }
 
+//Evento que escucha el boton 
 form.addEventListener("submit", function(e){
 
-e.preventDefault();
+    e.preventDefault();
 
-const nombre = document.getElementById("nombre").value.trim();
-const docente = document.getElementById("docente").value.trim();
-const duracion = document.getElementById("duracion").value.trim();
+    const nombre = document.getElementById("nombre").value.trim();
+    const docente = document.getElementById("docente").value.trim();
+    const duracion = document.getElementById("duracion").value.trim();
 
-if(nombre.length < 3 || docente.length < 5 || duracion === ""){
-error.textContent = "Complete todos los campos correctamente";
-return;
-}
+    //creacion de la tarjeta para los cursos
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-error.textContent = "";
+    //Insertar la informacion que llevara la tarjeta
+    card.innerHTML = `
+    <h3>${nombre}</h3>
+    <p>Docente: ${docente}</p>
+    <p>Duración: ${duracion}</p>
+    <button class="eliminar">Eliminar</button>
+    `;
 
-const card = document.createElement("div");
-card.classList.add("card");
+    //agrega la tarjeta al contenedor de cursos del index.html
+    lista.appendChild(card);
 
-card.innerHTML = `
-<h3>${nombre}</h3>
-<p>Docente: ${docente}</p>
-<p>Duración: ${duracion}</p>
-<button class="eliminar">Eliminar</button>
-`;
+    totalCursos++;
+    actualizarContador();
 
-lista.appendChild(card);
+    form.reset();
 
-totalCursos++;
-actualizarContador();
+    const btnEliminar = card.querySelector(".eliminar");
 
-form.reset();
-
-const btnEliminar = card.querySelector(".eliminar");
-
-btnEliminar.addEventListener("click", function(){
-card.remove();
-totalCursos--;
-actualizarContador();
-});
+    //evento para eliminar la tarjeta del curso
+    btnEliminar.addEventListener("click", function(){
+        card.remove();
+        totalCursos--;
+        actualizarContador();
+    });
 
 });
 
 
 const botones = document.querySelectorAll("nav button");
 
+//recorremos todos los botones del menu de navegacion 
 botones.forEach(boton=>{
-boton.addEventListener("click", ()=>{
+    boton.addEventListener("click", ()=>{
 
-document.querySelectorAll("main section").forEach(sec=>{
-sec.classList.add("hidden");
-});
+        document.querySelectorAll("main section").forEach(sec=>{
+            sec.classList.add("hidden");
+        });
 
-const id = boton.dataset.section;
-document.getElementById(id).classList.remove("hidden");
+    const id = boton.dataset.section;
+    document.getElementById(id).classList.remove("hidden");
 
-});
+    });
 });
